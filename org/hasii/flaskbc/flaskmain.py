3594545fc0af70a8b6b61e74b0fc14b8fb404fbb
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-07-11 16:59:12 -0500

    First commit

---
+++ org/hasii/flaskbc/flaskmain.py
@@ -0,0 +1,125 @@
+
+import json
+
+import logging
+import logging.config
+
+from uuid import uuid4
+
+from argparse import ArgumentParser
+
+import requests
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from flask import url_for
+
+from org.hasii.flaskbc.BlockChain import BlockChain
+
+
+JSON_LOGGING_CONFIG_FILENAME = "loggingConfiguration.json"
+
+
+def main():
+
+    with open(JSON_LOGGING_CONFIG_FILENAME, 'r') as loggingConfigurationFile:
+        configurationDictionary = json.load(loggingConfigurationFile)
+
+    logging.config.dictConfig(configurationDictionary)
+    logging.logProcesses = False
+    logging.logThreads   = False
+
+    logger = logging.getLogger(__name__)
+
+    logger.info(f"Starting")
+
+    app = Flask(__name__)
+
+    blockchain: BlockChain = BlockChain()
+
+    node_address = uuid4().hex  # Unique address for current node
+
+    parser = ArgumentParser()
+    parser.add_argument('-H', '--host', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default=6262, type=int)
+    args = parser.parse_args()
+
+    @app.route('/create-transaction', methods=['POST'])
+    def create_transaction():
+        transaction_data = request.get_json()  # Accepting Payload from user in JSON content type
+
+        index = blockchain.create_new_transaction(**transaction_data)
+
+        response = {
+            'message':    'Transaction has been submitted successfully',
+            'block_index': index
+        }
+
+        return jsonify(response), 201
+
+    @app.route('/mine', methods=['GET'])
+    def mine():
+        block = blockchain.mine_block(node_address)
+
+        response = {
+            'message':   'Successfully Mined the new Block',
+            'block_data': block
+        }
+        return jsonify(response)
+
+    @app.route('/chain', methods=['GET'])
+    def get_full_chain():
+        response = {
+            'chain': blockchain.get_serialized_chain
+        }
+        return jsonify(response)
+
+    @app.route('/register-node', methods=['POST'])
+    def register_node():
+        node_data = request.get_json()
+        blockchain.create_node(node_data.get('address'))
+        response = {
+            'message': '  New node has been added',
+            'node_count': len(blockchain.nodes),
+            'nodes':      list(blockchain.nodes),
+        }
+        return jsonify(response), 201
+
+    @app.route('/sync-chain', methods=['GET'])
+    def consensus():
+        neighbour_chains = get_neighbour_chains()
+        if not neighbour_chains:
+            return jsonify({'message': 'No neighbour chain is available'})
+
+        longest_chain = max(neighbour_chains, key=len)  # Get the longest chain
+
+        if len(blockchain.chain) >= len(longest_chain):  # If our chain is longest, then do nothing
+            response = {
+                'message': 'Chain is already up to date',
+                'chain':   blockchain.get_serialized_chain
+            }
+        else:  # If our chain isn't longest, then we store the longest chain
+            blockchain.chain = [blockchain.get_block_object_from_block_data(block) for block in longest_chain]
+            response = {
+                'message': 'Chain was replaced',
+                'chain':   blockchain.get_serialized_chain
+            }
+
+        return jsonify(response)
+
+    def get_neighbour_chains():
+        neighbour_chains = []
+
+        for node_addr in blockchain.nodes:
+            resp = requests.get(node_addr + url_for('get_full_chain')).json()
+            chain = resp['chain']
+            neighbour_chains.append(chain)
+
+        return neighbour_chains
+    app.run(host=args.host, port=args.port, debug=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/DistributedBlockChainTest.py
@@ -0,0 +1,61 @@
+
+from logging import getLogger
+from logging import Logger
+
+from test.BaseTest import BaseTest
+
+import requests
+
+
+class DistributedBlockChainTest(BaseTest):
+
+    @classmethod
+    def setUpClass(cls):
+        """"""
+        BaseTest.setUpLogging()
+
+    def setUp(self):
+        """"""
+
+        self.logger: Logger = getLogger(__name__)
+
+    def testDistributed(self):
+
+        server1: str = 'http://127.0.0.1:7575'
+        server2: str = 'http://127.0.0.1:9000'
+
+        self._register_node(server2, server1)  # server2 node will be register inside server1
+
+        self._create_transaction(server2, {'sender': 'I', 'recipient': 'Gabby10Meows', 'amount': 3})
+
+        self._mine_block(server2)  # Mined a new block on server2
+
+        self._get_server_chain(server1)  # server1's chain
+        self._get_server_chain(server2)  # server2's chain
+
+        self._sync_chain(server1)  # updating server1's chain with neighbour node's chain
+
+        self._get_server_chain(server1)  # server1's chain after syncing
+
+    def _register_node(self, node_addr, parent_server):
+        requests.post(parent_server + '/register-node', json={'address': node_addr})
+        self.logger.info(f"On Server {parent_server}: Node-{node_addr} has been registered successfully!")
+
+    def _create_transaction(self, server: str, data):
+        requests.post(server + '/create-transaction', json=data).json()
+        self.logger.info(f"On Server {server}: Transaction has been processed!")
+
+    def _mine_block(self, server: str):
+        requests.get(server + '/mine').json()
+        self.logger.info(f"On Server {server}: Block has been mined successfully!")
+
+    def _get_server_chain(self, server: str):
+        resp = requests.get(server + '/chain').json()
+        self.logger.info(f"On Server {server}: Chain is-{resp}")
+        return resp
+
+    def _sync_chain(self, server: str):
+        self.logger.info(f"On Server {server}: Started Syncing Chain . . .")
+        resp = requests.get(f"{server}/sync-chain")
+        self.logger.info("On Server {server}: Chain synced!")
+        self.logger.info(f"resp: {resp}")
